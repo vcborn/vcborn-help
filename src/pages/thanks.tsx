1bd191029cccbb040212e22104c55624878899af
wamo <ainznino@pm.me>
2024-06-30 23:26:48 +0900

    title

--- src/pages/thanks.tsx
+++ src/pages/thanks.tsx
@@ -1,8 +1,13 @@
 import Layout from '@/layouts/Layout'
+import Head from 'next/head'
 
 export default function Thanks() {
   return (
     <Layout search={false}>
+      <Head>
+        <title>お問い合わせありがとうございました | VCborn Support</title>
+        <meta name='robots' content='noindex' />
+      </Head>
       <div className='px-4 pb-60 max-w-3xl'>
         <h1 className='text-2xl font-bold'>お問い合わせありがとうございました</h1>
         <p className='mt-4'>
