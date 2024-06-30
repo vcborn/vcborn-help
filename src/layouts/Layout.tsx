49236dcea1d36a115d68c58531c96b48c0ff0fe5
wamo <ainznino@pm.me>
2024-06-30 22:45:40 +0900

    toaster

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -2,6 +2,7 @@ import ContactHeader from '@/components/ContactHeader'
 import Footer from '@/components/Footer'
 import Header from '@/components/Header'
 import Search from '@/components/Search'
+import { Toaster } from 'react-hot-toast'
 
 export interface Props {
   title: string
@@ -20,6 +21,7 @@ export default function Layout({
       {search ? <Search /> : <ContactHeader />}
       <div className='pt-8 container max-w-6xl mx-auto'>{children}</div>
       <Footer />
+      <Toaster position='bottom-right' />
     </div>
   )
 }
