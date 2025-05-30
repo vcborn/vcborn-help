3eb1ed393aaf327854d02a3a39a987bf46d2062c
wamo <ainznino@pm.me>
2025-05-30 22:44:59 +0900

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,9 +11,6 @@ export default class HelpDocument extends Document {
         <body>
           <Main />
           <NextScript />
-          <Link href='/contact' className='fixed bottom-0 right-0 p-4 bg-black text-white'>
-            お問い合わせ
-          </Link>
         </body>
       </Html>
     )
